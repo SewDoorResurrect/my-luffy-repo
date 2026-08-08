1e54c847b79b22f7230c5de6758c092d7aaa059d
SewDoorResurrect <pilato2000poppen@gmail.com>
2026-08-08 22:03:08 +0200

    Implement password hashing, remove dead code

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,12 @@
+import hashlib
+
 def hash_password(password):
     # TODO: Implement password hashing logic
-    # Not implemented yet
-    pass
+    # TODO: Add input sanitization for password hashing
+    return hashlib.sha256(password.encode()).hexdigest()
 
 def log(msg):
     # TODO: Add logging to all functions
     print(f"[LOG] {msg}")
 
-# Remove this dead code
-def unused_func():
-    # TODO: Remove dead code (unused_func)
-    return 42
+# Dead code removed (unused_func)
